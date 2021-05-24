2d9273f11871ed20427dc820fcb09b9140f7a0b5
jacoyutorius <jacoyutorius@gmail.com>
2021-05-24 23:32:27 +0900

    refs #1062 disabled vue-axe

--- plugins/axe.js
+++ plugins/axe.js
@@ -1,13 +1,14 @@
-import Vue from 'vue'
+// NOTE: 起動にどえらい時間がかかるので無効化(#1062)
+// import Vue from 'vue'
 
-if (process.env.NODE_ENV !== 'production') {
-  const VueAxe = require('vue-axe')
-  const AXE_LOCALE_JA = require('axe-core/locales/ja.json')
+// if (process.env.NODE_ENV !== 'production') {
+//   const VueAxe = require('vue-axe')
+//   const AXE_LOCALE_JA = require('axe-core/locales/ja.json')
 
-  Vue.use(VueAxe, {
-    config: {
-      locale: AXE_LOCALE_JA
-    },
-    clearConsoleOnUpdate: false
-  })
-}
+//   Vue.use(VueAxe, {
+//     config: {
+//       locale: AXE_LOCALE_JA
+//     },
+//     clearConsoleOnUpdate: false
+//   })
+// }
